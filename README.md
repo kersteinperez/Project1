e0adaf877a008237628a7d60a55355091302a350
Kerstein Perez <kersteinperez@Kersteins-MacBook-Pro.local>
2015-10-18 19:53:12 -0400

    Finalized js and added content to README

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Project1
+
+For this project I used HTML, CSS, vanilla JavaScript and JQuery.
+
+I used trello to keep track of my workflow, and mockflow to create a wireframe.
+
+Given the choice between Connect 4 and Blackjack, I assumed Connect 4 would be pretty straight forward after doing Tic Tac Toe for homework. It was only after plotting everything out that I realized the differences that would make it more difficult.
+
+Making a win was definitely the hardest part. I had to get help to figure that part out. I'm sure there is an easier more efficient way.
+
+If I had more time and knowledge I would do the following:
+1. Delay the prompts for player names. Whenever I tried, my board would stop accepting moves.
+2. Play a sound whenever a move is made.
+3. Have the game restart when done.
+4. When a winner is declared, I would want it to output the name the player entered at the beginning.
+5. I would also add opacity to the highlight column color of gold.
+6. I would make this reponsive
+
+Link to trello: https://trello.com/b/QqayF1kQ/project-1-connect-4
+
+Notes.md has additional information, useful when building this out.
--- Scripts/connect.js
+++ Scripts/connect.js
@@ -2,31 +2,16 @@ window.onload = function() {
   console.log("Game On.");
 
 
+// Alert: Player 1, what is your name?
+var reply = prompt("Playa 1, what's your name?", "")
+alert ( "Play on, playa " + reply + "!")
 
-//Alert: Player 1, what is your name?
-//var playerOneName=prompt('Playa One, Tell 'Em Ya Name':','');
 
-//Alert: Player 2, what is your name?
-//var playerTwoName=prompt('Playa Two, Tell 'Em Ya Name':','');
+// Alert: Player 2, what is your name?
+var reply = prompt("Playa 2, what's your name?", "")
+alert ( "Play on, playa " + reply + "!")
 
-//Highlights the column you are on
-function columnHighlightOn(column){
-  column.css({
-    "background-color": "gold",
-    /* I wanted to change the cursor so I knew when to click and when my turn was done, I looked this up. I probably could've prompted each player but I couldn't deal with the pop-ups anymore. */
-    "cursor": "pointer"
-  });
-};
-
-/* After turn, remove highlight and change */
-function columnHighlightOff(column){
-  column.css({
-    "background-color": "transparent",
-    "cursor": "default"
-  });
-};
-
-// check if cell is available (aka has grey)
+// Check if cell is available (does it have grey in it?)
 function checkColumn(column){
   var col = column.attr("id"),
       i = 1,
@@ -37,26 +22,44 @@ function checkColumn(column){
       return cell;
     };
   };
-  // if cell is not available
+  // If no available cell found
   return false;
 };
 
-//If it is red's turn and cell is available, remove grey and add red.
+// Highlights the column you are on
+function columnHighlightOn(column){
+  column.css({
+    "background-color": "gold",
+    /* I wanted to change the cursor so I knew when to click and when my turn was done. Had to look this up. I probably could've prompted each player but I  didn't want more pop-ups. */
+    "cursor": "pointer"
+  });
+};
+
+// After turn, remove highlight and change back cursor
+function columnHighlightOff(column){
+  column.css({
+    "background-color": "transparent",
+    "cursor": "default"
+  });
+};
+
+
 
   var isRedTurn = true;
 
 function colorCell(cell, isRedTurn){
   if (isRedTurn){
+    // If it is red's turn and cell is available, remove grey and add red.
     cell.removeClass("grey").addClass("red");
   } else {
-    //if it's not red's turn, and cell is available, remove grey and add black
+    // If it's not red's turn, and cell is available, remove grey and add blue
     cell.removeClass("grey").addClass("blue");
   };
 };
 
 
 
-/* I feel like I could've used arrays here but I started going down a path of checking on neighbors first and that led to directions. I had a lot of help with this part and am still a little shaky on how exactly it works */
+/* Check for winner. I feel like I could've used arrays here but I started going down a path of checking on neighbors first and that led to using directions. This took the longest time  */
 
 function checkWinner(cell, isRedTurn){
   var id = cell.attr("id").split("-"),
@@ -64,7 +67,7 @@ function checkWinner(cell, isRedTurn){
       row = id[1],
       color = isRedTurn ? "red" : "blue";
 
-  //check north-south
+  // Check north-south
   if (checkNorthSouth(col, row, color) ||
       checkWestEast(col, row, color) ||
       checkNorthwestSoutheast(col, row, color) ||
@@ -78,19 +81,19 @@ function checkNorthSouth(col, row, color){
   var rowN = row,
       rowS = row,
       inARow = 1;
-  //north
+  // North
   while (checkCell(col, ++rowN, color)){
     inARow++;
   };
-  //south
+  // South
   while (checkCell(col, --rowS, color)){
     inARow++;
   };
-  //winner?
+  // Winner?
   if (inARow >= 4){
     return true;
   };
-  //no north-south win
+  // No north-south win
   return false;
 };
 
@@ -98,19 +101,19 @@ function checkWestEast(col, row, color){
   var colW = col,
       colE = col,
       inARow = 1;
-  //west
+  // West
   while (checkCell(--colW, row, color)){
     inARow++;
   };
-  //east
+  // East
   while (checkCell(++colE, row, color)){
     inARow++;
   };
-  //winner?
+  // Winner?
   if (inARow >= 4){
     return true;
   };
-  //no west-east win
+  // No west-east win
   return false;
 };
 
@@ -120,19 +123,19 @@ function checkNorthwestSoutheast(col, row, color){
       rowNW = row,
       rowSE = row,
       inARow = 1;
-  //nw
+  // NW
   while (checkCell(--colNW, ++rowNW, color)){
     inARow++;
   };
-  //se
+  // SE
   while (checkCell(++colSE, --rowSE, color)){
     inARow++;
   };
-  //winner?
+  // Winner?
   if (inARow >= 4){
     return true;
   };
-  //no nw-se win
+  // No NW-SE win
   return false;
 };
 
@@ -142,54 +145,51 @@ function checkSouthwestNortheast(col, row, color){
       rowSW = row,
       rowNE = row,
       inARow = 1;
-  //sw
+  // SW
   while (checkCell(--colSW, --rowSW, color)){
     inARow++;
   };
-  //ne
+  // NE
   while (checkCell(++colNE, ++rowNE, color)){
     inARow++;
   };
-  //winner?
+  // Winner?
   if (inARow >= 4){
     return true;
   };
-  //no sw-ne win
+  // No SW-NE win
   return false;
 };
 
 function checkCell(col, row, color){
   if ($("#"+col+"-"+row).length){
-    //element exists, return true if the class color matches, false if not
+    // Element exists, return true if the class color matches, false if not
     return $("#"+col+"-"+row).hasClass(color);
   };
-  //element doesn't exist because it's off the board
+  // Element doesn't exist because it's off the board
   return false;
 };
 
 function endGame(isRedTurn){
-  //stop listening for column events
+  // Stop listening for column events
   $(".column").off("mouseover click");
-  //declare a winner
+  // Declare a winner, if the win is on red's turn then:
   if (isRedTurn){
-    alert("Red Wins!");
+    alert("Red, Takin A Win Back To The Streets!");
+  // If it is not on red's turn that means blue won
   } else {
-    alert("Blue Wins!");
+    alert("Blue, Takin A Win Back To The Streets!");
   };
 };
 
 
-
-
-
-
   $(".column").on("mouseover", function(){
-    //get next vacant cell
+    // Get next vacant cell
     var cell = checkColumn($(this));
     if (cell){
-      //change column background color
+      // Change column background color
       columnHighlightOn($(this));
-      //listen for click on column
+      // Listen for click on column
       $(this).on("click", function(){
         colorCell(cell, isRedTurn);
         if (checkWinner(cell, isRedTurn)){
@@ -197,6 +197,7 @@ function endGame(isRedTurn){
         } else {
           isRedTurn = !isRedTurn;
           columnHighlightOff($(this));
+          // Stop listening
           $(this).off("click");
         };
       });
